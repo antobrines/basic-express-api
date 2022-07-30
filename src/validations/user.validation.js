373b87c1bd31303fb7e71b96169e71b27ce0b586
Antonin BRINES <antonin.brines@outlook.fr>
2022-07-31 01:31:16 +0200

    fix: 🐛 body validation

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -1,19 +1,23 @@
 const Joi = require('Joi');
 
-const register = Joi.object().keys({
-  username: Joi.string()
-    .min(2)
-    .max(30)
-    .required(),
-  password: Joi.string().trim().min(6).required(),
-  confirm_password: Joi.string().valid(Joi.ref('password')).required(),
-  email: Joi.string().email().required()
-});
+const register = {
+  body: Joi.object().keys({
+    username: Joi.string()
+      .min(2)
+      .max(30)
+      .required(),
+    password: Joi.string().trim().min(6).required(),
+    confirm_password: Joi.string().valid(Joi.ref('password')).required(),
+    email: Joi.string().email().required()
+  })
+};
 
-const login = Joi.object().keys({
-  email: Joi.string().required(),
-  password: Joi.string().required(),
-});
+const login = {
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  })
+};
 
 module.exports = {
   register,
